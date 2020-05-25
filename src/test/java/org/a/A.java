5e61c9156bee4f117567ad1aa9ab24f91cd01a8b
divyadb97 <divya.dbivyas@gmail.com>
2020-05-25 22:04:08 +0530

    pushing

--- src/test/java/org/a/A.java
+++ src/test/java/org/a/A.java
@@ -1,9 +1,11 @@
 package org.a;
 
 public class A {
-	private void method() {
+	private void method() throws InterruptedException {
 		
 		System.out.println("one");
+		Thread.sleep(1000);
+		System.out.println("poj");
 	}
 
 }
